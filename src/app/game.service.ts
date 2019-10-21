fe8aa4ad31b0be611c8ca19ef4f48fcb4b488137
Olga Furmanowska <olgafurmanowska@gmail.com>
2019-10-21 18:55:42 +0200

    Add methods to check if the piece move is valid

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
-
+import { IPiece } from './../piece.component'
+import { COLS, ROWS } from  './constants'
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  valid(p: IPiece):boolean {
+    return p.shape.every((row, dy) => {
+      return row.every((value, dx) => {
+        let x = p.x + dx;
+        let y = p.y + dy;
+        return (
+          this.isEmpty(value) || 
+          (this.insideWalls(x) && this.aboveFloor(y))
+        );
+      });
+    });
+  }
 
-  constructor() { }
+  isEmpty(value: number):boolean {
+   return value === 0
+  }
+
+  insideWalls(x: number): boolean {
+    return x >= 0 && x < COLS;
+  }
+
+  aboveFloor(y: number): boolean {
+    return y <= ROWS
+  }
+
 }
