64ab543e36d38642abdf042cce02dfd922c4aafc
Olga Furmanowska <olgafurmanowska@gmail.com>
2019-10-21 18:34:03 +0200

    Add moves property and HostListener for defining basic moves for piece

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
-import { COLS, ROWS, BLOCK_SIZE } from './../constants';
+import { Component, ViewChild, ElementRef, OnInit, HostListener } from '@angular/core';
+import { COLS, ROWS, BLOCK_SIZE, KEY } from './../constants';
 import  { Piece, IPiece } from './../../piece.component'
 import { GameService } from '../game.service'
 
@@ -19,6 +19,33 @@ export class BoardComponent implements OnInit {
   level: number;
   board: number[][];
   piece: Piece;
+  moves = {
+    [KEY.LEFT]: (p: IPiece): IPiece => ({ ...p, x: p.x - 1}),
+    [KEY.RIGHT]: (p: IPiece): IPiece => ({ ...p, x: p.x + 1}),
+    [KEY.UP]: (p: IPiece): IPiece => ({ ...p, y: p.y + 1}),
+    [KEY.SPACE]: (p: IPiece): IPiece => ({ ...p, y: p.y + 1})
+  };
+
+  @HostListener('window:keydown', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+      if (event.keyCode === KEY.ESC) {
+         alert('Game over!')
+        //this.gameOver()
+      } else if (this.moves[event.keyCode]) {
+        event.preventDefault();
+        console.log(event.keyCode)
+        // Get new state
+        const p = this.moves[event.keyCode](this.piece);
+        // Move the piece
+        this.piece.move(p);
+        console.log(this.piece.move(p));
+        // Clear the old position before drawing
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        // Draw the new position
+        this.piece.draw();
+      }
+  }
+
 
   constructor(
     private gameService: GameService
@@ -37,6 +64,7 @@ export class BoardComponent implements OnInit {
     // Calculate size of canvas from constants.
     this.ctx.canvas.width = COLS * BLOCK_SIZE;
     this.ctx.canvas.height = ROWS * BLOCK_SIZE;
+    this.ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
   }
 
   getEmptyBoard(): number[][] {
