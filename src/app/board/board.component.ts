8066c901555acc2a4d921ffec92dc822c5eeb095
Olga Furmanowska <olgafurmanowska@gmail.com>
2019-10-21 16:34:58 +0200

    Add board component

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,34 @@
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { COLS, ROWS, BLOCK_SIZE } from './../constants'
+
+@Component({
+  selector: 'game-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+export class BoardComponent implements OnInit {
+  //Get reference to the canvas.
+  @ViewChild('board', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  ctx: CanvasRenderingContext2D;
+  points: number;
+  lines: number;
+  level: number;
+
+  ngOnInit() {
+    this.initBoard()
+  }
+
+  initBoard() {
+    // Get the 2D context that we draw on.
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+
+    // Calculate size of canvas from constants.
+    this.ctx.canvas.width = COLS * BLOCK_SIZE;
+    this.ctx.canvas.height = ROWS * BLOCK_SIZE;
+  }
+
+  play() {}
+
+}
