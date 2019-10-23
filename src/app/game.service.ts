2ee0fc82c4c937adcd74d05d0bf2d5fc938c8bee
Olga Furmanowska <olgafurmanowska@gmail.com>
2019-10-23 18:01:05 +0200

    Fix rotate method in gameService

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -6,14 +6,15 @@ import { COLS, ROWS, POINTS } from  './constants'
 })
 export class GameService {
   valid(p: IPiece, board: number[][]):boolean {
-0;
     return p.shape.every((row, dy) => {
       return row.every((value, dx) => {
         let x = p.x + dx;
         let y = p.y + dy;
         return (
           this.isEmpty(value) || 
-          (this.insideWalls(x) && this.aboveFloor(y) && this.notOccupied(board, x, y))
+          (this.insideWalls(x) && 
+          this.aboveFloor(y) && 
+          this.notOccupied(board, x, y))
         );
       });
     });
@@ -37,8 +38,8 @@ export class GameService {
 
   rotate(piece: IPiece): IPiece {
     let p: IPiece = JSON.parse(JSON.stringify(piece));
-    for (let y  = 0; y < p.shape.length; y++) {
-      for (let x = 0; x < y; x++) {
+    for (let y  = 0; y < p.shape.length; ++y) {
+      for (let x = 0; x < y; ++x) {
         [p.shape[x][y], p.shape[y][x]] = [p.shape[y][x], p.shape[x][y]]
       }
     }
