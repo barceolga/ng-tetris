3519a9abe6d86baa465deab8a9aefcf3ed7e1a0c
Olga Furmanowska <olgafurmanowska@gmail.com>
2019-10-23 15:24:03 +0200

    Update method for getting points after clearing lines (depending on current level)

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -46,11 +46,18 @@ export class GameService {
     return p;
   }
 
-  getLinesClearedPoints(lines: number): number {
-    return lines === 1 ? POINTS.SINGLE :
-           lines === 2 ? POINTS.DOUBLE :
-           lines === 3 ? POINTS.TRIPLE :
-           lines === 4 ? POINTS.TETRIS : 0;
+  getLinesClearedPoints(lines: number, level: number): number {
+    const lineClearPoints =
+     lines === 1
+          ? POINTS.SINGLE :
+          lines === 2 
+          ? POINTS.DOUBLE :
+          lines === 3 
+          ? POINTS.TRIPLE :
+          lines === 4 
+          ? POINTS.TETRIS : 
+          0;
+      return (level + 1) * lineClearPoints;
   }
 
 }
